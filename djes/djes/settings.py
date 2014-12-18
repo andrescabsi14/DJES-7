ea165d21c9c6e1f1e851e1f7efee19096148ebd0
Andres Cabrera <andrescabsi@gmail.com>
2014-12-18 16:53:29 -0500

    south installation settings

--- djes/djes/settings.py
+++ djes/djes/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    ##Dependencies
+    'south',
     ##Custom APPS
     'lessons',
     'courses',
