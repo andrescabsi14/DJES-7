5073d22199dbaf6d024bb73ae46a192bedda0a76
Andres Cabrera <andrescabsi@gmail.com>
2014-12-18 18:56:09 -0500

    Print data v1
    Print data with mustache syntax without ajax

--- djes/lessons/views.py
+++ djes/lessons/views.py
@@ -4,7 +4,9 @@ from django.http import HttpResponse, Http404
 from .models import Lesson
 
 def lesson_view(request, title):
+	##Return 404 if not exist
 	lesson = get_object_or_404(Lesson, title=title)
-
-	return HttpResponse('Ok...')
-
+	##Custom Variables for view
+	bio = lesson.teacher.profile
+	##Return lesson if exist
+	return render(request, 'lesson.html', {'lesson': lesson, 'bio': bio})
