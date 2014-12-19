feb2bdbb8faab4f0e47d2534ad19f60f889ffe15
Andres Cabrera <andrescabsi@gmail.com>
2014-12-19 07:30:45 -0500

    admin customizing

--- djes/lessons/admin.py
+++ djes/lessons/admin.py
@@ -2,4 +2,11 @@ from django.contrib import admin
 
 from .models import Lesson
 
-admin.site.register(Lesson)
+class TrackAdmin(admin.ModelAdmin):
+	#List View Attr of model
+	list_display = ('order', 'image', 'title', 'teacher', 'course')
+	#List Filter Attr of model
+	list_filter = ('teacher', 'course')
+	#Search
+	search_fields = ('title', 'teacher')
+admin.site.register(Lesson, TrackAdmin)
